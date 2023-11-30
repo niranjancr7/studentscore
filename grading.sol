3661b999b64068d4f6028c734ee426723442b7b7
niranjancr7 <152554795+niranjancr7@users.noreply.github.com>
2023-12-01 02:15:10 +0530

    Update grading.sol

--- grading.sol
+++ grading.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ExamGrading {
+    address public teacher;
+    mapping(address => uint256) public pupilScores;
+
+    constructor() {
+        teacher = msg.sender;
+    }
+
+    function submitScore(address pupil, uint256 score) public {
+        // Use require() for validation checks
+        require(msg.sender == teacher, "Only the teacher can submit scores");
+
+        // Use assert() to ensure a specific condition
+        assert(score >= 0 && score <= 100);
+
+        // Use revert() for exceptional cases
+        if (pupilScores[pupil] > 0) {
+            revert("Score already submitted for the pupil");
+        }
+
+        pupilScores[pupil] = score;
+    }
+}
 
